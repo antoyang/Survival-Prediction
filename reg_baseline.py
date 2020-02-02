3d3491d1b8b220cff19e9fd6b49eeefbb8ed8539
Leon Zheng <leonpapanoel@gmail.com>
2020-02-02 18:03:56 +0100

    Reg baseline

--- reg_baseline.py
+++ reg_baseline.py
@@ -13,52 +13,48 @@ import numpy as np
 from metrics import cindex
 
 
-radiomics_train = pd.read_csv("data/train/features/radiomics.csv", index_col=0, usecols=[0, 5, 32, 39, 6])
+radiomics_train = pd.read_csv("data/train/features/radiomics.csv", index_col=0, usecols=[0, 5, 32, 39, 6, 3])
 clinical_train = pd.read_csv("data/train/features/clinical_data.csv", index_col=0, usecols=[0, 3])
+# added_features = pd.read_csv("data/train/features/train_extracted_features.csv", index_col='patient_number', usecols=[])
+# print(added_features)
 input_train = pd.concat([radiomics_train, clinical_train], axis=1)
 output_train = pd.read_csv("data/train/y_train.csv", index_col=0, header=0)
 
-# """ Linear Regression """
-# linear_reg = linear_model.LinearRegression()
-# linear_reg.fit(X_train, Y_train[:, 0])
-# Y_pred = linear_reg.predict(X_test)
-# print(f'Mean squared error: {mean_squared_error(Y_test[:, 0], Y_pred)}')
-# print(concordance_index_censored(Y_test[:, 1].astype('bool'), Y_test[:, 0], Y_pred))
+""" CoxPH Regression """
+X_train, X_test, Y_train, Y_test = model_selection.train_test_split(input_train, output_train, test_size=0.21)
+print(X_train)
+print(Y_train)
 
-# """ CoxPH Regression """
-# X_train, X_test, Y_train, Y_test = model_selection.train_test_split(input_train, output_train, test_size=0.21)
-# print(X_train)
-# print(Y_train)
-#
-# # Training
-# strct_Y_train = Surv.from_dataframe('Event', 'SurvivalTime', Y_train)
-# print(strct_Y_train)
-# coxph_reg = CoxPHSurvivalAnalysis(alpha=10)
-# coxph_reg.fit(X_train, strct_Y_train)
-#
-# # Prediction
-# prediction = coxph_reg.predict_survival_function(X_test)
-# threshold = 0.5
-# y_pred = []
-# for pred in prediction:
-#     time = pred.x
-#     survival_prob = pred.y
-#     i_pred = 0
-#     while i_pred < len(survival_prob) - 1 and survival_prob[i_pred] > threshold:
-#         i_pred += 1
-#     print(time[i_pred])
-#     y_pred.append(time[i_pred])
-# Y_pred = pd.DataFrame(np.array([[y, np.nan] for y in y_pred]), index=X_test.index, columns=['SurvivalTime', 'Event'])
-# print(Y_pred)
-#
-# # Score
-# print(cindex(Y_test, Y_pred))
+# Training
+strct_Y_train = Surv.from_dataframe('Event', 'SurvivalTime', Y_train)
+print(strct_Y_train)
+coxph_reg = CoxPHSurvivalAnalysis(alpha=10)
+coxph_reg.fit(X_train, strct_Y_train)
+
+# Prediction
+prediction = coxph_reg.predict_survival_function(X_test)
+threshold = 0.5
+y_pred = []
+for pred in prediction:
+    time = pred.x
+    survival_prob = pred.y
+    i_pred = 0
+    while i_pred < len(survival_prob) - 1 and survival_prob[i_pred] > threshold:
+        i_pred += 1
+    y_pred.append(time[i_pred])
+Y_pred = pd.DataFrame(np.array([[y, np.nan] for y in y_pred]), index=X_test.index, columns=['SurvivalTime', 'Event'])
+print(Y_pred)
+
+# Score
+print(cindex(Y_test, Y_pred))
 
 """ Prediction """
 # Test set
 radiomics_test = pd.read_csv("data/test/features/radiomics.csv", index_col=0, usecols=[0, 3, 5, 32, 39, 6])
 clinical_test = pd.read_csv("data/test/features/clinical_data.csv", index_col=0, usecols=[0, 3])
-input_test = pd.concat([radiomics_test, clinical_test], axis=1)
+# dum = pd.get_dummies(clinical_test['SourceDataset'], prefix='SourceDataset')
+# print(dum)
+input_test = pd.concat([radiomics_test, clinical_test['Nstage']], axis=1)
 print(input_test)
 
 # Training on the training set
@@ -77,7 +73,6 @@ for pred in prediction:
     i_pred = 0
     while i_pred < len(survival_prob) - 1 and survival_prob[i_pred] > threshold:
         i_pred += 1
-    print(time[i_pred])
     y_pred.append(time[i_pred])
 submission = pd.DataFrame(np.array([[y, np.nan] for y in y_pred]), index=input_test.index, columns=['SurvivalTime', 'Event'])
 print(submission)
