889ed90aef1f7e9e91ba0e0154457c34569e25a9
Leon Zheng <leonpapanoel@gmail.com>
2020-02-02 17:34:00 +0100

    Baseline 0.67

--- reg_baseline.py
+++ reg_baseline.py
@@ -13,12 +13,9 @@ import numpy as np
 from metrics import cindex
 
 
-radiomics = pd.read_csv("data/train/features/radiomics.csv", index_col=0, usecols=[0, 5, 32, 39, 6])
-# print(radiomics)
-clinical = pd.read_csv("data/train/features/clinical_data.csv", index_col=0, usecols=[0, 3])
-# print(clinical.head())
-input_train = pd.concat([radiomics, clinical], axis=1)
-# print(input_train.head())
+radiomics_train = pd.read_csv("data/train/features/radiomics.csv", index_col=0, usecols=[0, 5, 32, 39, 6])
+clinical_train = pd.read_csv("data/train/features/clinical_data.csv", index_col=0, usecols=[0, 3])
+input_train = pd.concat([radiomics_train, clinical_train], axis=1)
 output_train = pd.read_csv("data/train/y_train.csv", index_col=0, header=0)
 
 # """ Linear Regression """
@@ -28,37 +25,62 @@ output_train = pd.read_csv("data/train/y_train.csv", index_col=0, header=0)
 # print(f'Mean squared error: {mean_squared_error(Y_test[:, 0], Y_pred)}')
 # print(concordance_index_censored(Y_test[:, 1].astype('bool'), Y_test[:, 0], Y_pred))
 
-""" CoxPH Regression """
-X_train, X_test, Y_train, Y_test = model_selection.train_test_split(input_train, output_train, test_size=0.21)
-print(X_train)
-print(Y_train)
-
-# Training
-strct_Y_train = Surv.from_dataframe('Event', 'SurvivalTime', Y_train)
-print(strct_Y_train)
-coxph_reg = CoxPHSurvivalAnalysis(alpha=1e3)
-coxph_reg.fit(X_train, strct_Y_train)
-
-# Prediction
-prediction = coxph_reg.predict(X_test)
-prediction = 100 * (prediction - np.min(prediction) + 1e-3)
-Y_pred = pd.DataFrame(np.array([[y, np.nan] for y in prediction]), index=X_test.index, columns=['SurvivalTime', 'Event'])
-print(Y_pred)
-
-# Score
-print(cindex(Y_test, Y_pred))
-
-""" Prediction """
-strct_Y_data = Surv.from_arrays(Y_data[:, 1].astype('bool'), Y_data[:, 0])
-print(strct_Y_data)
-# coxph_reg.fit(X_data, strct_Y_data)
-# input_test = pd.read_csv("data/test/features/radiomics.csv", index_col=0, header=[0,1])
-# X_data_test = input_test.to_numpy()
-# X_data_test = preprocessing.scale(X_data_test)
-# Y_pred = coxph_reg.predict(X_data_test)
+# """ CoxPH Regression """
+# X_train, X_test, Y_train, Y_test = model_selection.train_test_split(input_train, output_train, test_size=0.21)
+# print(X_train)
+# print(Y_train)
+#
+# # Training
+# strct_Y_train = Surv.from_dataframe('Event', 'SurvivalTime', Y_train)
+# print(strct_Y_train)
+# coxph_reg = CoxPHSurvivalAnalysis(alpha=10)
+# coxph_reg.fit(X_train, strct_Y_train)
+#
+# # Prediction
+# prediction = coxph_reg.predict_survival_function(X_test)
+# threshold = 0.5
+# y_pred = []
+# for pred in prediction:
+#     time = pred.x
+#     survival_prob = pred.y
+#     i_pred = 0
+#     while i_pred < len(survival_prob) - 1 and survival_prob[i_pred] > threshold:
+#         i_pred += 1
+#     print(time[i_pred])
+#     y_pred.append(time[i_pred])
+# Y_pred = pd.DataFrame(np.array([[y, np.nan] for y in y_pred]), index=X_test.index, columns=['SurvivalTime', 'Event'])
 # print(Y_pred)
 #
-# # Write submission
-# submission = pd.DataFrame([[y, 'nan'] for y in Y_pred], columns=['SurvivalTime', 'Event'], index=input_test.index)
-# print(submission.head())
-# submission.to_csv('submission.csv')
+# # Score
+# print(cindex(Y_test, Y_pred))
+
+""" Prediction """
+# Test set
+radiomics_test = pd.read_csv("data/test/features/radiomics.csv", index_col=0, usecols=[0, 3, 5, 32, 39, 6])
+clinical_test = pd.read_csv("data/test/features/clinical_data.csv", index_col=0, usecols=[0, 3])
+input_test = pd.concat([radiomics_test, clinical_test], axis=1)
+print(input_test)
+
+# Training on the training set
+strct_Y_train = Surv.from_dataframe('Event', 'SurvivalTime', output_train)
+# print(strct_Y_train)
+coxph_reg = CoxPHSurvivalAnalysis(alpha=10)
+coxph_reg.fit(input_train, strct_Y_train)
+
+# Prediction
+prediction = coxph_reg.predict_survival_function(input_test)
+threshold = 0.55
+y_pred = []
+for pred in prediction:
+    time = pred.x
+    survival_prob = pred.y
+    i_pred = 0
+    while i_pred < len(survival_prob) - 1 and survival_prob[i_pred] > threshold:
+        i_pred += 1
+    print(time[i_pred])
+    y_pred.append(time[i_pred])
+submission = pd.DataFrame(np.array([[y, np.nan] for y in y_pred]), index=input_test.index, columns=['SurvivalTime', 'Event'])
+print(submission)
+
+# Write submission
+submission.to_csv('submission.csv')
