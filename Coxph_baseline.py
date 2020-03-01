39763bb15f00a1323191c3267fb1cebd3305a009
Leon Zheng <leonpapanoel@gmail.com>
2020-03-01 20:01:42 +0100

    Baseline model written with Scikit learn Estimator. Cross validation, grid search.

--- Coxph_baseline.py
+++ Coxph_baseline.py
@@ -5,32 +5,40 @@ Leon Zheng
 
 import pandas as pd
 from Coxph_regression import CoxPhRegression
+from sklearn.model_selection import GridSearchCV
+import numpy as np
+
+"""
+Feature selection
+"""
+radiomics_features = ['PatientID', 'original_shape_Sphericity', 'original_shape_SurfaceVolumeRatio',
+                      'original_shape_Maximum3DDiameter', 'original_glcm_JointEntropy', 'original_glcm_Id',
+                      'original_glcm_Idm']
+clinical_features = ['PatientID', 'SourceDataset', 'Nstage']
 
 """
 Reading data
 """
 # Read training set
-radiomics_train = pd.read_csv("data/train/features/radiomics.csv", index_col=0, usecols=[0, 5, 32, 39, 6, 3])
-clinical_train = pd.read_csv("data/train/features/clinical_data.csv", index_col=0, usecols=[0, 3])
-# added_features = pd.read_csv("data/train/features/train_extracted_features.csv", index_col='patient_number', usecols=[])
-# print(added_features)
+radiomics_train = pd.read_csv("data/train/features/radiomics.csv", index_col=0, usecols = radiomics_features)
+clinical_train = pd.read_csv("data/train/features/clinical_data.csv", index_col=0, usecols = clinical_features)
+clinical_train.SourceDataset = pd.factorize(clinical_train.SourceDataset)[0]
 input_train = pd.concat([radiomics_train, clinical_train], axis=1)
+print(input_train)
 output_train = pd.read_csv("data/train/y_train.csv", index_col=0, header=0)
+print(output_train)
 
 # Read testing set
-radiomics_test = pd.read_csv("data/test/features/radiomics.csv", index_col=0, usecols=[0, 3, 5, 32, 39, 6])
-clinical_test = pd.read_csv("data/test/features/clinical_data.csv", index_col=0, usecols=[0, 3])
-# dum = pd.get_dummies(clinical_test['SourceDataset'], prefix='SourceDataset')
-# print(dum)
-input_test = pd.concat([radiomics_test, clinical_test['Nstage']], axis=1)
+radiomics_test = pd.read_csv("data/test/features/radiomics.csv", index_col=0, usecols= radiomics_features)
+clinical_test = pd.read_csv("data/test/features/clinical_data.csv", index_col=0, usecols= clinical_features)
+clinical_test.SourceDataset = pd.factorize(clinical_test.SourceDataset)[0]
+input_test = pd.concat([radiomics_test, clinical_test], axis=1)
 
 """ 
 Grid search
 """
-from sklearn.model_selection import GridSearchCV
-
-tuned_params = {"alpha": [0.01, 0.1, 1, 10],
-                "threshold": [0.2, 0.4, 0.6, 0.8]}
+tuned_params = {"alpha": np.logspace(-8, -3, 10),
+                "threshold": np.linspace(0.85, 0.95, 10)}
 grid_search = GridSearchCV(CoxPhRegression(), tuned_params, cv=5, n_jobs=4)
 grid_search.fit(input_train, output_train)
 print(grid_search.best_score_)
@@ -41,8 +49,7 @@ print(best_params)
 Create submission
 """
 # Create submission
-coxph = CoxPhRegression()
-coxph.set_params(**best_params)
+coxph = CoxPhRegression(**best_params)
 coxph.fit(input_train, output_train)
 y_pred = coxph.predict(input_test)
 y_pred.to_csv('submission.csv')
--- Coxph_regression.py
+++ Coxph_regression.py
@@ -43,28 +43,3 @@ class CoxPhRegression(BaseEstimator):
     def score(self, X, y):
         y_pred = self.predict(X)
         return cindex(y, y_pred)
-
-
-if __name__ == '__main__':
-
-    from sklearn.model_selection import cross_val_score
-    # Read training set
-    radiomics_train = pd.read_csv("data/train/features/radiomics.csv", index_col=0, usecols=[0, 5, 32, 39, 6, 3])
-    clinical_train = pd.read_csv("data/train/features/clinical_data.csv", index_col=0, usecols=[0, 3])
-    # added_features = pd.read_csv("data/train/features/train_extracted_features.csv", index_col='patient_number', usecols=[])
-    # print(added_features)
-    input_train = pd.concat([radiomics_train, clinical_train], axis=1)
-    output_train = pd.read_csv("data/train/y_train.csv", index_col=0, header=0)
-
-    coxph = CoxPhRegression()
-    # cindex_score = make_scorer(cindex)
-    print(cross_val_score(coxph, input_train, output_train, cv=5))
-
-    # Grid search
-    from sklearn.model_selection import GridSearchCV
-    tuned_params = {"alpha": [0.01, 0.1, 1, 10],
-                    "threshold": [0.2, 0.4, 0.6, 0.8]}
-    grid_search = GridSearchCV(CoxPhRegression(), tuned_params, cv=5, n_jobs=4)
-    grid_search.fit(input_train, output_train)
-    print(grid_search.best_score_)
-    print(grid_search.best_params_)
